1c89d6bedbd05bd24136c5b3d299b431ce17eed3
adarshbandi <adas2211@gmail.com>
2020-07-26 15:26:18 +0530

    Updated

---
+++ Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public static string Audience = "https://localhost:44391/";
+        public static string Issuer = Audience;
+        public static string Secret = "not_too_short_secret_otherwise_its_easy_to_crackdown";
+    }
+}
---
+++ Server/Controlles/OAuthController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace Server.Controlles
+{
+    public class OAuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Authorize(
+            string response_type, // authorization flow type
+            string client_id,
+            string redirect_uri,
+            string scope, // what info i want *email,phone,cookie
+            string state) // random string generated to confirm we are going back to the same client
+        {
+            var query = new QueryBuilder();
+            query.Add("redirectUri", redirect_uri);
+            query.Add("state", state);
+
+            return View(model: query.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Authorize(
+            string username,
+            string redirectUri,
+            string state)
+        {
+            const string code = "BLAHBLAHBLAH";
+            var query = new QueryBuilder();
+            query.Add("code", code);
+            query.Add("state", state);
+
+            return Redirect($"{redirectUri}{query.ToString()}");
+        }
+
+        public async Task<IActionResult> Token(
+            string grant_type, // flow of access_token request
+            string code, // confirmation of the authentication process
+            string redirect_uri,
+            string client_id)
+        {
+            // some mechanism for validating code
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "some_id"),
+                new Claim("Granny", "cookie")
+            };
+
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audience,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMilliseconds(1),
+                signingCredentials);
+
+            var access_token = new JwtSecurityTokenHandler().WriteToken(token);
+
+            var responseObject = new
+            {
+                access_token,
+                token_type = "Bearer",
+                raw_claim = "oauthTutorial"
+            };
+
+            var responseJson = JsonConvert.SerializeObject(responseObject);
+            var responseBytes = Encoding.UTF8.GetBytes(responseJson);
+
+            await Response.Body.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+            return Redirect(redirect_uri);
+        }
+
+        [Authorize]
+        public IActionResult Validate()
+        {
+            if (HttpContext.Request.Query.TryGetValue("access_token", out var accessToken))
+            {
+
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
